b6d642d49bc9dc299ef2ba4c46b236bf199dae26
Soichiro Kashima <soichiro.kashima@gmail.com>
2016-11-26 02:34:14 +0900

    Add chart for each tasks

--- js/load.js
+++ js/load.js
@@ -14,22 +14,15 @@
       $tbody.append($row);
     });
 
-    let ganttFrom = new Date();
-    ganttFrom.setFullYear(2016);
-    ganttFrom.setMonth(10);
-    ganttFrom.setDate(1);
-    let ganttTo = new Date();
-    ganttTo.setFullYear(ganttFrom.getFullYear());
-    ganttTo.setMonth(ganttFrom.getMonth());
-    ganttTo.setDate(ganttFrom.getDate());
-    ganttTo.setMonth(ganttTo.getMonth() + 1);
+    let ganttFrom = moment('2016-11-01', 'YYYY-MM-DD');
+    let ganttTo = ganttFrom.clone().add(1, 'months');
     let $ganttTable = $('.wbs.script .gantt table');
     let $ganttThead = $ganttTable.find('thead');
     let $row = $('<tr></tr>');
     let days = 0;
-    for (let i = ganttFrom; i <= ganttTo; i.setDate(i.getDate() + 1)) {
-      $row.append(`<th>${(i.getMonth() + 1)}/${i.getDate()}</th>`);
+    for (let i = ganttFrom.clone(); i.isSameOrBefore(ganttTo); i.add(1, 'days')) {
       days++;
+      $row.append(`<th class="gantt-${days} d-${i.month()}-${i.date()}">${i.month()+1}/${i.date()}</th>`);
     }
     $ganttThead.append($row);
 
@@ -40,6 +33,15 @@
         $row.append('<td>&#160;</td>');
       }
       $ganttTbody.append($row);
+    });
+    let dayWidth = $('.gantt-1').outerWidth();
+    let dayHeight = $ganttTable.find('tbody tr td').outerHeight();
+    data.tasks.forEach(function(e, idx) {
+      if (e.startAt && e.endAt) {
+        let start = moment(e.startAt).diff(ganttFrom, 'days');
+        let duration = moment(e.endAt, 'YYYY-MM-DD').diff(moment(e.startAt, 'YYYY-MM-DD'), 'days') + 1;
+        $ganttTable.append(`<div class="bar" style="top: ${32 + dayHeight * idx}px; left: ${dayWidth * start}px; height: ${dayHeight - 1}px; width: ${dayWidth * duration}px;"></div>`);
+      }
     });
   }
 })();
